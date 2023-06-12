ab8acfb49a7c8c2d2528fa28ae73abeeebbee961
Renat <natanat2@mail.ru>
2023-06-13 02:53:20 +0500

    code 70%

--- krest.py
+++ krest.py
@@ -1,4 +1,3 @@
-26/51
 pole = [[' '] * 3 for i in range(3)]
 igrok = "X"
 schet = 1
@@ -11,7 +10,17 @@ def hod():
     global igrok
     global schet
     while True:
-        x, y = map(int, input('Выбирите номер столбца и строки в значении от 0-2 через пробел: ').split())
+        coordynaty = input('Выбирите номер столбца и строки в значении от 0-2 через пробел: ').split()
+        if len(coordynaty) != 2:
+            print('Вы ввели неверное число')
+            continue
+        else:
+            x, y = coordynaty
+        if not(x.isdigit()) or not (y.isdigit()):
+            print('Введите числа!')
+            continue
+        x, y = int(x), int(y)
+
         if 0 <= x < 3 and 0 <= y < 3:
             if pole[y][x] == " ":
                 pole[y][x] = igrok
