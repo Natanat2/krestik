b09938248bb109016cc35605aed6ea92e8efdcb4
Renat <natanat2@mail.ru>
2023-06-13 13:19:19 +0500

    ready

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 pole = [[' '] * 3 for i in range(3)]
 igrok = "X"
 schet = 1
+def win():
+    winline = [((0,0),(0,1),(0,2)),((1,0),(1,1),(1,2)),((2,0),(2,1),(2,2)),
+               ((0,0),(1,0),(2,0)),((0,1),(1,1),(2,1)),((0,2),(1,2),(2,2)),
+               ((0,0),(1,1),(2,2)),((0,2),(1,1),(2,0))
+               ]
+    for line in winline:
+        a = line[0]
+        b = line[1]
+        c = line[2]
+        if pole[a[0]][a[1]] == pole[b[0]][b[1]] == pole[c[0]][c[1]] !=" ":
+            print(f'Выиграл {pole[a[0]][a[1]]}')
+            return True
+    return False
 def razmetka():
     print(f'    0   1   2')
     for i in range(3):
@@ -10,7 +23,7 @@ def hod():
     global igrok
     global schet
     while True:
-        coordynaty = input('Выбирите номер столбца и строки в значении от 0-2 через пробел: ').split()
+        coordynaty = input('Выбирите номер строки и столбца в значении от 0-2 через пробел: ').split()
         if len(coordynaty) != 2:
             print('Вы ввели неверное число')
             continue
@@ -22,15 +35,16 @@ def hod():
         x, y = int(x), int(y)
 
         if 0 <= x < 3 and 0 <= y < 3:
-            if pole[y][x] == " ":
-                pole[y][x] = igrok
+            if pole[x][y] == " ":
+                pole[x][y] = igrok
                 razmetka()
+                if win():
+                    return
                 schet += 1
                 if schet % 2 == 0:
                     igrok = '0'
                 else:
                     igrok = 'X'
-
             else:
                 print('Поле занято')
         else:
