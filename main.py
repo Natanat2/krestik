0e1e413b10bfc928a6edbf4ad94687f237055c60
Renat <natanat2@mail.ru>
2023-06-13 13:51:50 +0500

    ready

--- main.py
+++ main.py
@@ -2,24 +2,32 @@ pole = [[' '] * 3 for i in range(3)]
 igrok = "X"
 schet = 1
 print('Добро пожаловать в игру \n    крестики-нолики\nвводите поле в формате x, y\nгде x - строка, y - столбец')
+
+
 def win():
-    winline = [((0,0),(0,1),(0,2)),((1,0),(1,1),(1,2)),((2,0),(2,1),(2,2)),
-               ((0,0),(1,0),(2,0)),((0,1),(1,1),(2,1)),((0,2),(1,2),(2,2)),
-               ((0,0),(1,1),(2,2)),((0,2),(1,1),(2,0))
+    winline = [((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+               ((0, 0), (1, 0), (2, 0)), ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)),
+               ((0, 0), (1, 1), (2, 2)), ((0, 2), (1, 1), (2, 0))
                ]
     for line in winline:
         a = line[0]
         b = line[1]
         c = line[2]
-        if pole[a[0]][a[1]] == pole[b[0]][b[1]] == pole[c[0]][c[1]] !=" ":
+        if pole[a[0]][a[1]] == pole[b[0]][b[1]] == pole[c[0]][c[1]] != " ":
             print(f'Выиграл {pole[a[0]][a[1]]}')
             return True
     return False
+
+
 def razmetka():
     print(f'    0   1   2')
     for i in range(3):
         print(f'{i} | {" | ".join(pole[i])} |')
+
+
 razmetka()
+
+
 def hod():
     global igrok
     global schet
@@ -29,8 +37,8 @@ def hod():
             print('Вы ввели неверное число')
             continue
         else:
-            x, y = coordynaty #присвоение координат, можно сменить столбцы на строки
-        if not(x.isdigit()) or not (y.isdigit()):
+            x, y = coordynaty  # присвоение координат, можно сменить столбцы на строки
+        if not (x.isdigit()) or not (y.isdigit()):
             print('Введите числа!')
             continue
         x, y = int(x), int(y)
@@ -53,5 +61,6 @@ def hod():
         if schet == 10:
             print('Ничья')
             break
+
 
 hod()
